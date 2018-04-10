8509c8bbac6db227a951c30e5dd07a7e3a70560a
Morozov Damian <web@morozov33.ru>
2018-04-10 16:28:55 +0300

    [10.04.2018]
    Update buttonGenerics_Click

--- Version2/Form2.cs
+++ Version2/Form2.cs
@@ -31,18 +31,12 @@ namespace Version2
 }" + Environment.NewLine);
             log.AppendText("private class SomeClass { }" + Environment.NewLine + Environment.NewLine);
 
-            var listInt = new GenericList<int>();
-            listInt.Add(10);
             log.AppendText("var listInt = new GenericList<int>();" + Environment.NewLine);
             log.AppendText("listInt.Add(10);" + Environment.NewLine + Environment.NewLine);
 
-            var listStr = new GenericList<string>();
-            listStr.Add("some string");
             log.AppendText("var listStr = new GenericList<string>();" + Environment.NewLine);
             log.AppendText("listStr.Add(\"some string\");" + Environment.NewLine + Environment.NewLine);
 
-            var listSomeClass = new GenericList<SomeClass>();
-            listSomeClass.Add(new SomeClass());
             log.AppendText("var listSomeClass = new GenericList<SomeClass>();" + Environment.NewLine);
             log.AppendText("listSomeClass.Add(new SomeClass());" + Environment.NewLine + Environment.NewLine);
         }
